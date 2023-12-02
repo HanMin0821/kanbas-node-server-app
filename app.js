351113d9b2070879cd12f5c48222f3a902006764
Han Min <minhan0821@outlook.com>
2023-12-01 17:22:09 -0800

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -5,12 +5,39 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
-
-
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/Kanbas';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(
+    session(sessionOptions)
+);
+
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+UserRoutes(app);
 
 ModuleRoutes(app);
 CourseRoutes(app);
